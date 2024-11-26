aadcd0f73b5af949636ef127306c2796d3ddaad2
sveabjoorn <h23svebj@du.se>
2024-11-26 10:56:53 +0100

    26-11-2024

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-document.getElementById('divStyle');
-//let checkBox;
+let checkBox = document.getElementById('divStyle');
+let text = document.getElementsByClassName('textfield');
+let button = document.querySelector('#btn');
+let emptyDiv = document.getElementById('empty');
+
+// variabel test
+/* console.log(button);
+console.log(checkBox);
+console.log(text);
+console.log(emptyDiv); */
+
+
+// Uppgift 5
+eventListener('ett', 'två');
+
+function eventListener(param1, param2) {
+    console.log('Funktion: eventListener, parametrar', param1, param2);
+    return 'Returnerar ' + param1 + ' ' + param2;
+}
+console.log(eventListener('console','log'));
