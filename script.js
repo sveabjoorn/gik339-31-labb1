c997c1b777c5c471b351623074d0b484f198886f
sveabjoorn <h23svebj@du.se>
2024-12-03 13:29:25 +0100

    03-12-2024

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const outputField = document.getElementById('output');
 //Funktionen är en anonym? arrowfunktion?
 textFields.forEach((field) => field.addEventListener('blur', handleBlur));
 
-const output = document.getElementById('output');
-//Funktionsdeklaration
-function handleBlur(e) {
-    const name = e.target.name;
-    const value = e.target.value;    
-    const html = `<p>Innehållet  ${name} har värdet ${value} </p>`;
-   // const html = `<p>${content}</p>`;
-    output.insertAdjacentHTML('beforeend', html);
-    console.log('TextFields genererade eventet ', e);
-}
 
 //Uppgift 6
     
@@ -28,6 +18,40 @@ button.addEventListener('click', (e) =>
 // Lägger till eventlyssnare för checkboxen
 checkBox.addEventListener('change', (e) => 
     console.log('Checkboxen ändrades'));
+
+
+// const output = document.getElementById('output');
+//Funktionsdeklaration
+function handleBlur(e) {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;    
+    //const html = `<p>Innehållet  ${name} har värdet ${value} </p>`;
+    const html = `<p>${value}</p>`;
+    outputField.insertAdjacentHTML('beforeend', html);
+    console.log('TextFields genererade eventet ', e);
+}
+
+// const display = document.getElementById('outputField');
+function handleClick(e) {
+    e.preventDefault();
+    const targetName = e.target.id;
+    const colorField = settings.color;
+    const contentField = settings.content;
+    console.log(targetName);
+
+
+    if (targetName === 'divStyle') {
+        const newElement = document.createElement('div');
+        newElement.classList.add('new-element');
+        newElement.style.backgroundColor = colorField.value;
+        newElement.innerHTML = contentField.value;
+
+        outputField.insertAdjacentElement('beforeend', newElement);
+    }
+}
+
+
 
 
 
